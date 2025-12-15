889ce7eea23652fc4eef1af098288e9675d605c6
Kaido Hussar <kaidohus@gmail.com>
2025-12-15 08:48:00 +0200

    up

--- app3/src/i18n/index.ts
+++ app3/src/i18n/index.ts
@@ -2,8 +2,8 @@ import { createContentstorageI18n } from '@contentstorage/vue-i18n-plugin'
 import contentStorageConfig from '../../contentstorage.config.js'
 
 import en from './locales/EN.json'
-import es from './locales/ES.json'
 import fr from './locales/FR.json'
+import es from './locales/ES.json'
 
 const savedLanguage = localStorage.getItem('selectedLanguage') || 'en'
 
