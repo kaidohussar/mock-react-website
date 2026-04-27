7d4e6ba19b9084a5518cb3977a7e37f2540c8c6a
Kaido Hussar <kaidohus@gmail.com>
2026-04-27 13:04:41 +0300

    up

--- app1/src/App.tsx
+++ app1/src/App.tsx
@@ -17,6 +17,7 @@ import './styles/main.scss'
 import { ContentstorageIntlProvider } from '@contentstorage/react-intl-plugin'
 import enMessages from './content/json/EN.json'
 import etMessages from './content/json/ET.json'
+import { useAuth } from './hooks/useAuth'
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const LocaleContext = createContext<{
@@ -28,12 +29,8 @@ export const LocaleContext = createContext<{
 })
 
 const App: React.FC = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const { isAuthenticated } = useAuth()
   const [locale, setLocale] = useState('en')
-
-  const handleLoginSuccess = () => {
-    setIsLoggedIn(true)
-  }
 
   const messages = locale === 'et' ? etMessages : enMessages
 
@@ -47,11 +44,8 @@ const App: React.FC = () => {
         <Router>
           <div className="App">
             <Routes>
-              <Route
-                path="/login"
-                element={<LoginPage onLoginSuccess={handleLoginSuccess} />}
-              />
-              {isLoggedIn ? (
+              <Route path="/login" element={<LoginPage />} />
+              {isAuthenticated ? (
                 <Route path="/" element={<Layout />}>
                   <Route path="dashboard" element={<DashboardPage />} />
                   <Route path="reports" element={<ReportsPage />} />
